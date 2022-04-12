c30e1e79da37c861838d99b3b9d978070b60e38b
MikiasL <101825463+MikiasL@users.noreply.github.com>
2022-04-11 22:41:27 -0700

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Outputs the values
  *
@@ -16,16 +15,13 @@ int main(void)
 			for (l = m =n =1; l < 10; l++)
 			{
 				
-					putchar((n%10) + '0');
-					putchar((m%10) + '0');
-					putchar((l%10) + '0');
+					putchar((n % 10) + '0');
+					putchar((m % 10) + '0');
+					putchar((l % 10) + '0');
 					if (n == 7 && m == 9 && l == 10)
-					{
 						continue;
 						putchar(',');
 						putchar(' ');
-					}
-				
 			}
 		}
 	}
