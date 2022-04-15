916170e27011107b442f8c689e1f7b9e6eda7bb0
MikiasL <101825463+MikiasL@users.noreply.github.com>
2022-04-15 16:44:27 +0300

    Task - 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - checks for lowercase character.
+ * @a: entero recibo por main.
+ * @b: entero recibido por main
+ * Return: 1 if c is uppercase
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
